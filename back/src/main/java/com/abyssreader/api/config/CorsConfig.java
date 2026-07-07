5ff85a0e506094f233e6c7e50aab622f8042daba
MaxonHLP <maxiceverino@gmail.com>
2026-07-06 22:39:29 -0300

    Cambios en Config

--- back/src/main/java/com/abyssreader/api/config/CorsConfig.java
+++ back/src/main/java/com/abyssreader/api/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
-        config.setAllowCredentials(false);
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
